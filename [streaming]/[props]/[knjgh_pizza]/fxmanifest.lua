1f33137049b31f23713e14b91775dea8fd1d96b3
Haze <70538228+HazeWorksOnFiveMServer@users.noreply.github.com>
2023-07-30 11:41:03 +0200

    Delete [streaming] directory

--- [streaming]/[props]/[knjgh_pizza]/fxmanifest.lua
+++
@@ -1,9 +0,0 @@
-fx_version 'adamant'
-games { 'gta5' }
-author 'KNJGH'
-description 'PIZZA_SLICES'
-version '1.0.0'
-
-
--- Loading YTYP (important for the script)
-data_file 'DLC_ITYP_REQUEST' 'stream/knjgh_pizzas.ytyp'
